4bbc0be2ba8d54b985d42d8dbcd92c98af235a47
Alejandro Palacios <apalac99@gmail.com>
2025-02-22 19:47:44 -0500

    1

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Iniciar el servidor
+// Iniciar el servidor en el puerto 3000
 app.listen(3000, () => console.log('Servidor corriendo en http://localhost:3000'));
